727f81acc8e330843df6cfcbd0fa5ac59330a6bf
Anton Subbotin <batonsson@gmail.com>
2021-04-26 15:47:51 +0300

    Fix broken component

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -7,15 +7,6 @@
       <input type="password" placeholder="Password" id="password" />
     </div>
     <div class="field">
-      <button class="button" type="submit">Submit</butto<template>
-  <form class="form" @submit="onSubmitForm">
-    <div class="field">
-      <input type="text" placeholder="Login" id="login" />
-    </div>
-    <div class="field">
-      <input type="password" placeholder="Password" id="password" />
-    </div>
-    <div class="field">
       <button class="button" type="submit">Submit</button>
     </div>
   </form>
@@ -23,7 +14,8 @@
 
 <script>
 export default {
-  name: 'SignInForm',
+  name: 'SignUpForm',
+
   methods: {
     onSubmitForm(e) {
       e.preventDefault()
@@ -36,38 +28,11 @@ export default {
 .field:not(:last-child) {
   margin-bottom: 2em;
 }
+
 .field * {
   width: 100%;
 }
-.field input {
-  padding: 0.5em 0;
-  border: none;
-  border-bottom: 1px solid #00000042;
-}
-</style>
-n>
-    </div>
-  </form>
-</template>
 
-<script>
-export default {
-  name: 'SignInForm',
-  methods: {
-    onSubmitForm(e) {
-      e.preventDefault()
-    }
-  }
-}
-</script>
-
-<style>
-.field:not(:last-child) {
-  margin-bottom: 2em;
-}
-.field * {
-  width: 100%;
-}
 .field input {
   padding: 0.5em 0;
   border: none;
